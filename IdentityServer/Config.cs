146a49bc8f0a9bb53169ca1c34b5ee5ac1a8dba1
Victor Bolum <victor.bolum@cyberspace.net.ng>
2020-12-17 19:12:01 +0100

    init

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,74 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using static IdentityModel.OidcConstants;
+
+namespace ids4
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResource("custom", "Custom Properties", new List<string>{"role", "id" } )
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("api1", "My API"),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            {
+                new Client{ ClientName = "Binlist Api", ClientId = "BinlistApi", 
+                    
+                    AllowedGrantTypes = IdentityServer4.Models.GrantTypes.ClientCredentials , 
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:4566/signin-oidc"
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes =
+                    {
+                        "api1"
+                    },
+                    AccessTokenLifetime = 3600 * 24 * 7, // 7 days
+                    IdentityTokenLifetime = 3600 
+                },
+                new Client{ ClientName = "Client 2", ClientId = "Client2",
+
+                    AllowedGrantTypes =IdentityServer4.Models.GrantTypes.CodeAndClientCredentials ,
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:44328/signin-oidc"
+                    },
+                    AllowedScopes =
+                    {
+                        "openid","profile", "custom"
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    RequirePkce = false,
+                    RequireConsent = true,
+                    PostLogoutRedirectUris = new List<string>()
+                    {
+                        "https://localhost:44328/signout-callback-oidc"
+                    }
+                },
+            };
+    }
+}
---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServerHost.Quickstart.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ids4
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IWebHostEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // uncomment, if you want to add an MVC-based UI
+            services.AddControllersWithViews();
+
+            var connString = "Data Source = localhost; Initial Catalog = ids4; Integrated Security = true; ";
+
+            var builder = services.AddIdentityServer(options =>
+            {
+                // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+                options.EmitStaticAudienceClaim = true;
+            })
+                //.AddInMemoryIdentityResources(Config.IdentityResources)
+                //.AddInMemoryApiScopes(Config.ApiScopes)
+                //.AddInMemoryClients(Config.Clients)
+                .AddTestUsers(TestUsers.Users)
+                ;
+
+            var migrationAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+            builder.AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = builder => builder.UseSqlServer(connString, options => options.MigrationsAssembly(migrationAssembly));
+            });
+
+            builder.AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = builder => builder.UseSqlServer(connString, options => options.MigrationsAssembly(migrationAssembly));
+            });
+
+
+
+
+            // not recommended for production - you need to store your key material somewhere secure
+            builder.AddDeveloperSigningCredential();
+            //builder.AddSigningCredential(LoadCertificateFromStore());
+        }
+
+        public X509Certificate2 LoadCertificateFromStore()
+        {
+            string thumbPrint = "pastethubmprinthere";
+
+            using (var store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                var certCollection = store.Certificates.Find(X509FindType.FindByThumbprint, thumbPrint, true);
+                if (certCollection.Count == 0)
+                {
+                    throw new System.Exception("the specified certificate was not found");
+                }
+                return certCollection[0];
+            }
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // uncomment if you want to add MVC
+            app.UseStaticFiles();
+            app.UseRouting();
+            
+            app.UseIdentityServer();
+
+            // uncomment, if you want to add MVC
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
